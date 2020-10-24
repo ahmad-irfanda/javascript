798b8d297bfdae9f997bd84a9cdb88a096596815
ahmad-irfanda <selalumoveon@gmail.com>
2020-10-24 23:38:46 +0700

    Add 4 File: urutan-operator, operator spread, dll

---
+++ urutan-operator.js
@@ -0,0 +1,13 @@
+// urutan operator didalam javascript berdasarkan prioritas : 
+// ()
+// * / %
+// + -
+// == != === !==
+// &&
+// ||
+
+var x = 10 + 3 * 10;
+console.log(x);
+
+var y = (10 + 3) * 10;
+console.log(y);
