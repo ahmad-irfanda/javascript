cf583fee15db83d6e1bcc46cf884c2fedcc4d9e1
ahmad-irfanda <selalumoveon@gmail.com>
2020-10-24 22:54:05 +0700

    menambahkan file aritmatika, logika, operator, dll

---
+++ operators.js
@@ -0,0 +1,8 @@
+function panggilOperators(){
+  let a = 4;
+  let b = 3;
+
+  return a*b;
+}
+
+console.log(panggilOperators());
---
+++ typeof.js
@@ -0,0 +1,18 @@
+//Mengecek Tipe Data
+function checkTipeData(){
+  var data1 = "irfanda";
+  var data2 = 5000;
+  var data3 = [1,2,3,4,5];
+  var data4 = {
+    nama: 'budi',
+    usia: '20',
+    tinggi: '175'
+  };
+
+  console.log(typeof(data1));
+  console.log(typeof(data2));
+  console.log(typeof(data3));
+  console.log(typeof(data4));
+}
+
+checkTipeData();
